bf67dd4b087e7ba12957749f93fdeb7cffd5c8e9
Luke Davis <lukedavis@google.com>
2018-05-15 10:18:01 -0700

    Update Readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Rather than diegesis.
 
 You'll need to install the chromedriver for selenium to work. Instructions can be found [here](https://www.kenst.com/2015/03/installing-chromedriver-on-mac-osx/)
+
+Be sure to **Not** install 2.9 I know that will seem like the most recent version, but it's actually from 2014. I don't really know what crazy semver system they're using, but I when I installed, the most recent version was [2.38](https://chromedriver.storage.googleapis.com/index.html?path=2.38/) (from April of 2018).
--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
+    console.log('driver execution begun');
     await driver.get('http://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
+    console.log('driver execution complete');
     await driver.quit();
   }
 })();
+
+/*
+var driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com/ncr')
+    .then(() => {
+      console.log('driver engaged'); 
+      driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    })
+    .then(() => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
+    .then(() => driver.quit());
+*/
